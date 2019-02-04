ec5b59629ad3246c0f003abb8dcdc0ec29b92438
bluworld <bluworld@gmail.com>
2019-02-03 21:45:26 -0800

    change to actionBar/Floating Action Button layout

--- app/src/main/java/com/arkletech/videoplayer/MainActivity.java
+++ app/src/main/java/com/arkletech/videoplayer/MainActivity.java
@@ -9,14 +9,19 @@ import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Build;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -45,9 +50,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-//        EditText et = findViewById(R.id.et_url);
-//        et.setText(video_url);
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show();
+//            }
+//        });
+
         CheckBox cb_hideTitle = findViewById(R.id.cb_hide_title);
         cb_hideTitle.setChecked(true);
         CheckBox cb_hideStatus = findViewById(R.id.cb_hide_status);
@@ -81,6 +95,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mActv.setOnClickListener(this);
 
         mVideoPlayer = new Intent(getApplicationContext(), PlayVideoActivity.class);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -202,6 +239,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     .show();
         }
     }
+
 
     protected void updateAdapterUrlEntries()
     {
